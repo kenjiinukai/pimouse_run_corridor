2221813fa25b138dffedcba1c484e44336d523f4
Kenji Inukai <kenji.inki@gmail.com>
2020-03-16 03:04:31 +0000

    Add the wall trace node

--- test/travis_test_wall_stop_accel.py
+++ test/travis_test_wall_stop_accel.py
@@ -3,7 +3,7 @@ import unittest, rostest
 import rosnode, rospy
 import time
 
-class WallStopTest(unittest.TestCase):
+class WallStopAccelTest(unittest.TestCase):
     def set_and_get(self,lf,ls,rs,rf):
         with open("/dev/rtlightsensor0","w") as f:
             f.write("%d %d %d %d\n" % (rf,rs,ls,lf))
@@ -33,6 +33,6 @@ class WallStopTest(unittest.TestCase):
 
 if __name__ == '__main__':
     time.sleep(3)
-    rospy.init_node('travis_test_wall_stop')
-    rostest.rosrun('pimouse_run_corridor','travis_test_wall_stop',WallStopTest)
+    rospy.init_node('travis_test_wall_stop_accel')
+    rostest.rosrun('pimouse_run_corridor','travis_test_wall_stop_accel',WallStopAccelTest)
 
