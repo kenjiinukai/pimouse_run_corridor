1d9da19f703ae745f3495be24a13936308e9c56b
Kenji Inukai <kenji.inki@gmail.com>
2020-03-16 04:24:30 +0000

    Fix bugs of wall trace node and test

--- scripts/wall_around.py
+++ scripts/wall_around.py
@@ -4,7 +4,7 @@ from geometry_msgs.msg import Twist
 from std_srvs.srv import Trigger, TriggerResponse
 from pimouse_ros.msg import LightSensorValues
 
-class WallTrace():
+class WallAround():
     def __init__(self):
         self.cmd_vel = rospy.Publisher('/cmd_vel',Twist,queue_size=1)
 
@@ -30,16 +30,24 @@ class WallTrace():
 	data.linear.x = 0.3
 	data.angular.z = 0.0
 	while not rospy.is_shutdown():
-		if self.wall_front
+		if self.wall_front(self.sensor_values):
+			data.angular.z = - math.pi
+		elif self.too_right(self.sensor_values):
+			data.angular.z = math.pi
+		elif self.too_left(self.sensor_values):
+			data.angular.z = - math.pi
+		else:
+			e = 50 - self.sensor_values.left_side
+			data.angular.z = e * math.pi / 180
 
 		self.cmd_vel.publish(data)
 		rate.sleep()
 
 if  __name__ == '__main__':
-    rospy.init_node('wall_trace')
+    rospy.init_node('wall_around')
     rospy.wait_for_service('/motor_on')
     rospy.wait_for_service('/motor_off')
     rospy.on_shutdown(rospy.ServiceProxy('/motor_off',Trigger).call)
     rospy.ServiceProxy('/motor_on',Trigger).call()
-    WallTrace().run()
+    WallAround().run()
 
